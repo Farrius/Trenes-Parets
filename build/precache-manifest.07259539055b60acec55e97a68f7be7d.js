0dd0dbe4befd9aac1680ad69a39fd289f44862f1
Farrius <oscargarries@gmail.com>
2019-12-25 21:05:30 +0100

    commit para production

---
+++ build/precache-manifest.07259539055b60acec55e97a68f7be7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262bb51cb6f09c5346fc160f67c966a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13e3dfe7dc664dea83bd",
+    "url": "/static/css/main.b48b23b7.chunk.css"
+  },
+  {
+    "revision": "336e30c36aaad87f6cf3",
+    "url": "/static/js/2.74140907.chunk.js"
+  },
+  {
+    "revision": "13e3dfe7dc664dea83bd",
+    "url": "/static/js/main.4d447604.chunk.js"
+  },
+  {
+    "revision": "b9026e427e6f58605902",
+    "url": "/static/js/runtime-main.07e53d85.js"
+  }
+]);
